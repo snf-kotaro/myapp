da33ca684a34bee6e773f2ae999b71902edc56c4
高橋　浩太郎　K.T <k_takahashi@saice.net>
2023-08-08 16:48:03 +0900

    modify title message (Sinatra -> Kotaro)

--- myapp.rb
+++ myapp.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-    @title = 'Hello Sinatra!'
+    @title = 'Hello Kotaro!'
     @subtitle = 'Welcome to the world of sinatra and ruby.'
     erb :index
 end
