fe325706263d8bd501da1bc0d8b805a1b7e6e194
高橋　浩太郎　K.T <k_takahashi@saice.net>
2023-08-08 16:28:09 +0900

    modify myapp (add about page)

--- myapp.rb
+++ myapp.rb
@@ -2,7 +2,11 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-    @title = 'Hello Kotaro!'
+    @title = 'Hello Sinatra!'
     @subtitle = 'Welcome to the world of sinatra and ruby.'
     erb :index
+end
+
+get '/about' do
+    erb :about
 end
